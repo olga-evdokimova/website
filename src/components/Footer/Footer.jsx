9efab0566e89cde80d0f257ea88407c976f603b0
olga-evdokimova <pavedom13@gmail.com>
2023-02-10 17:44:31 +0300

    footer готов с адаптивом

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,16 @@ import Social from "../Social/Social";
 const Footer = () => {
   return (
     <footer className="footer">
-      <Logo></Logo>
-      <Social></Social>
+      <div className="footer__inner">
+        <Logo></Logo>
+        <Social></Social>
+      </div>
+      <div className="footer__copy">
+        <span>© 2023 </span>
+        <a href="https://github.com/olga-evdokimova">
+          Developed by Olga Evdokimova
+        </a>
+      </div>
     </footer>
   );
 };
